9fd89a31ba66ee4f34c64e7b6c776b16adaa4f08
Aniruddha Adak <adakaniruddha9@gmail.com>
2023-11-10 22:04:05 +0530

    add link for my profile page

--- src/app/links/page.js
+++ src/app/links/page.js
@@ -1,27 +1,33 @@
- 
 import Link from "next/link";
 
 export default function Page_links() {
   return (
     <>
+    {/* I used bootstrap here  */}
+      <div className=" btn btn-group">
+        {/* Link for Home page  */}
+        <Link href="/">
+          <button className="btn btn-outline-primary ">Home page </button>
+        </Link>
 
-    <div className=" btn btn-group ">
-  {/* Link for Home page  */}
-      <Link href="/">
-      <button className="btn btn-primary m-2">Home page </button> 
-       </Link>
+        {/* Links for link page  */}
+        <Link href="/links">
+          <button className="btn btn-outline-success ">
+            Links page
+          </button>
+        </Link>
 
-{/* Links for link page  */}
-       <Link href="/links">
-       <button className="btn btn-success m-2 transition-all">Links page</button>
-       </Link>
- 
- {/* Links for Error page  */}
- <Link href="/not-found/error">
- <button className="btn btn-danger m-2" >Error page</button>
-       </Link>
+        {/* Links for Error page  */}
+        <Link href="/error">
+          <button className="btn btn-outline-danger ">Error page</button>
+        </Link>
 
-</div>
+        {/* links for profile page  */}
+        <Link href="/profile">
+          <button className="btn btn-outline-info  ">Profile page</button>
+        </Link>
+
+      </div>
     </>
   );
 }
