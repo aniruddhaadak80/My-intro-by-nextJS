7ca3cffee9cc730e2eb82894b9c5e0417317aa86
Aniruddha Adak <adakaniruddha9@gmail.com>
2023-11-12 14:16:00 +0530

    commit

--- src/app/components/Addusers.js
+++ src/app/components/Addusers.js
@@ -1,9 +1,13 @@
-export default function AddUsers(){
-    return(
-        <>
-            <h1>User Details :</h1>
-            <input className="user-input" type="text" placeholder="Enter user details..."/>
-            <button className="btn btn-outline-warning">Add users</button>
-        </>
-    )
+export default function AddUsers() {
+  return (
+    <>
+      <h1>User Details :</h1>
+      <input
+        className="user-input"
+        type="text"
+        placeholder="Enter user details..."
+      />
+      <button className="btn btn-outline-warning">Add users</button>
+    </>
+  );
 }
--- src/app/components/DisplayUsers.js
+++ src/app/components/DisplayUsers.js
@@ -1,11 +1,10 @@
-export default function DisplayUsers(){
-    return(
-        <>
-        <div className="users-div">
-           <h1>Users List </h1>
-           <hr/>
-           
-           </div>
-        </>
-    )
+export default function DisplayUsers() {
+  return (
+    <>
+      <div className="users-div">
+        <h1>Users List </h1>
+        <hr />
+      </div>
+    </>
+  );
 }
